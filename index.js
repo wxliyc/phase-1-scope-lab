8bc2a452dbc47bfa89d7005a99a70f2a9d4cdcaf
Ethan Lee <wxliyc@gmail.com>
2022-02-25 11:03:51 -0600

    commit tree 0e1d20f3727ce6a338422169406458e23dc5be75

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){  
+    customerName.toUpperCase()
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    
+
+}
